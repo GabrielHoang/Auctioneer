8ea5dc8edd812870e22d06393dac7cb5eb66c78d
Gabriel Hoang <gabrielhoang.jr@gmail.com>
2019-11-14 02:32:38 +0100

    remove test files

--- src/main/java/auctioneer/HelloInterface.java
+++
@@ -1,8 +0,0 @@
-package auctioneer;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloInterface extends Remote {
-    public String say(String message) throws RemoteException;
-}
